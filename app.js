8daf960903f796aaa79e4d5c7cde33209afcca25
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-04-24 15:59:17 -0400

    trigger initial api call upon loading app using event listener

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // Initialise weather class
 const weather = new Weather("Boston", "MA");
 
-weather
-  .getWeather()
-  .then((results) => {
-    console.log(results);
-  })
-  .catch((err) => console.log(err));
+// Get weather on DOM load
+document.addEventListener("DOMContentLoaded", getWeather);
+
+function getWeather() {
+  weather
+    .getWeather()
+    .then((results) => {
+      console.log(results);
+    })
+    .catch((err) => console.log(err));
+}
