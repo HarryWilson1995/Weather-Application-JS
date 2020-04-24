bf46cf69700797ebc62cea40fef9bc09420f9081
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-04-24 15:24:10 -0400

    adding necessary markup with link and script tags to html file

--- weather.js
+++ weather.js
@@ -1,1 +1,7 @@
 fetch(https://www.weatherbit.io/api)
+
+https://www.weatherbit.io/api/weather-current
+
+my_key = 47f305e987b04dc5888192c9d95f22d9 
+
+example_request = https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=API_KEY 
