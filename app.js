fffd3a46117da4039c5ad49a172b97a49f7920e4
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-04-26 11:23:51 -0400

    adding change location functionality

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const ui = new UI();
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
 
+// Change location event
+document.getElementById("w-change-btn").addEventListener("click", (e) => {
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+
+  weather.changeLocation("Miami", "FL");
+
+  // Get and display weather
+  getWeather();
+  $("#locModal").modal("hide");
+});
+
 function getWeather() {
   weather
     .getWeather()
